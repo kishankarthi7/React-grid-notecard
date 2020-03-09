87fa26f4e65c2b5cec704dc493ab75ddd5cffb85
KISHAN L <kishan.karthi7@gmail.com>
2020-03-09 14:46:48 +0100

    resizing as map is dragged

--- src/components/MapLeaflet.js
+++ src/components/MapLeaflet.js
@@ -10,8 +10,9 @@ class MapLeaflet extends React.Component {
 
   render() {
     const position = [this.state.lat, this.state.lng];
+
     return (
-      <Map className="map" center={position} zoom={this.state.zoom}>
+      <Map id="map" center={position} zoom={this.state.zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/components/MyFirstGrid.js
+++ src/components/MyFirstGrid.js
@@ -8,34 +8,37 @@ import MapLeaflet from './MapLeaflet';
 class MyFirstGrid extends React.Component {
   render() {
     return (
-      <GridLayout
-        className="layout"
-        cols={4}
-        rowHeight={30}
-        width={1200}
-        draggableCancel="input, textarea"
-        compactType="horizontal"
-      >
-        <div
-          key="a"
-          data-grid={{ x: 0, y: 0, w: 0.9, h: 7.25, isResizable: true }}
+      <div className="grid">
+        <GridLayout
+          className="layout"
+          cols={4}
+          rowHeight={30}
+          width={1200}
+          draggableCancel="input, textarea"
+          compactType="horizontal"
         >
-          <NoteCard />
-        </div>
-        <div
-          key="b"
-          data-grid={{ x: 0, y: 0, w: 0.9, h: 7.25, isResizable: true }}
-        >
-          <NoteCard />
-        </div>
+          <div
+            key="a"
+            data-grid={{ x: 0, y: 0, w: 0.9, h: 7.25, isResizable: true }}
+          >
+            <NoteCard />
+          </div>
+          <div
+            key="b"
+            data-grid={{ x: 0, y: 0, w: 0.9, h: 7.25, isResizable: true }}
+          >
+            <NoteCard />
+          </div>
 
-        <div
-          key="c"
-          data-grid={{ x: 0, y: 0, w: 0.9, h: 7.25, isResizable: true }}
-        >
-          <MapLeaflet />
-        </div>
-      </GridLayout>
+          <div
+            className="map-container"
+            key="c"
+            data-grid={{ x: 0, y: 0, w: 0.9, h: 7.25, isResizable: true }}
+          >
+            <MapLeaflet />
+          </div>
+        </GridLayout>
+      </div>
     );
   }
 }
